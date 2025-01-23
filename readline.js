bc3f4df0322202ada9ee917a4e943ad7c12acb3c
Marcos Roberto <mr.codeengineer@gmail.com>
2025-01-23 20:11:11 -0300

    exclusão de comentários imprecisos

--- readline.js
+++ readline.js
@@ -1,7 +1,4 @@
-//a condição será a seguinte se eu sair trabalhar de manha, tarde e noite eu recebo 100 reais ganhos por dia 
-//se eu trabalhar só de manha e tarde ou de tarde noite recebo 70 reais
-//se eu trabalhar só de manha ou só de tarde ou só de noite eu tiro 40 reais
-//se eu não trabalhar, eu não ganho nada e preciso gastar.
+
 
 const readline = require('readline');
 
